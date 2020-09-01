fac02459fbfb8bc99583f46beccc36a36f702719
alisa318 <69658397+alisa318@users.noreply.github.com>
2020-09-01 16:22:58 +0800

    Update The Roasting Machine.py

--- The Roasting Machine.py
+++ The Roasting Machine.py
@@ -1,1 +1,8 @@
+import random 
 
+name = str(input("name :"))
+Choose_Insult = ["Too bad you can't photoshop your ugly personality", "So if i type 'idiot' into Google, would your picture come up?", "Someday you'll go far, and I hope you stay there"]
+for i in range(1,3) : 
+    chosen = random.choice(Choose_Insult)
+    print (name,",", chosen)
+    Choose_Insult.remove(chosen)
