9c55d90e824de2562ed1dcf2b8e3d3644c49c607
alisa318 <69658397+alisa318@users.noreply.github.com>
2020-09-02 08:15:13 +0800

    Update LAZY REPORT WRITER.py

--- LAZY REPORT WRITER.py
+++ LAZY REPORT WRITER.py
@@ -2,6 +2,6 @@ import random
 
 Students = int(input("number of students :" ))
 Choose_Comment = ["Improve handwriting", "Elaborate on this point", "Why?", "Explain further", "What else could this suggest?"]
-for i in range (1,Students) : 
+for i in range (Students) : 
   chosen = random.choice(Choose_Comment)
   print(chosen) 
