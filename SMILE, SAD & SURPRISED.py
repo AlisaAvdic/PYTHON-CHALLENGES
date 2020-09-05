680ff349b588affeb43fb41dc7a21ffb85c6f663
alisa318 <69658397+alisa318@users.noreply.github.com>
2020-09-05 16:49:54 +0800

    Create SMILE, SAD & SURPRISED.py

---
+++ SMILE, SAD & SURPRISED.py
@@ -0,0 +1,36 @@
+def happyface() : 
+  print("      _____     ")
+  print("   .'       '.  ")
+  print("  /   o   o   \ ")
+  print(" |             |")
+  print(" |   \     /   |")
+  print("  \   '---'   /")
+  print("   '. _____ .'")
+
+h = happyface()
+print(h)
+
+def sadface() : 
+  print("      _____     ")
+  print("   .'       '.  ")
+  print("  /   o   o   \ ")
+  print(" |   ;     ;   |")
+  print(" |     ---     |")
+  print("  \  /     \   /")
+  print("   '. _____ .'")
+
+s = sadface()
+print(s)
+
+def surprisedface() : 
+  print("      _____     ")
+  print("   .' ^   ^ '.")
+  print("  /   o   o   \ ")
+  print(" |   \     /   |")
+  print(" |      0      |")
+  print("  \           /")
+  print("   '. _____ .'")
+
+sr = surprisedface()
+print(sr)
+
